f30dea81209e213c4bb038b861bb07b2cb84222c
abssourav <abubakkarsiddique1781@gmail.com>
2024-03-16 15:08:59 +0600

    something changes

--- src/components/TrendingGames.jsx
+++ src/components/TrendingGames.jsx
@@ -6,7 +6,7 @@ const TrendingGames = ({ allGameList }) => {
             <h2 className='font-bold dark:text-white text-2xl mb-5'>Trending Games</h2>
             <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 '>
                 {allGameList?.map((item, index) => {
-                    if (index < 4) {
+                    if (index < 22) {
                         return (
                             <div key={index} className='bg-[#425572] rounded-lg cursor-pointer group hover:scale-110 transition-all duration-300 ease-in-out'>
                                 <img src={item.background_image} className='h-[270px] object-cover rounded-t-lg ' />
